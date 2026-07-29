148c4d7fcd688d7b57119262af29ff889a3bf5c1
Rory Coleman <rory.cosmic@yahoo.com>
2026-07-29 16:40:38 +1000

    as

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      keep_fnames: true,
+    },
+  },
 })
+// vite.config.ts
